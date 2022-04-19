fixed scale calculation bug. added scaling to subject images

BoxTheJets/scripts/normalize_subject_size.py
@@ -3,10 +3,12 @@ from astropy.io import ascii
 from astropy.table import Table
 from skimage import io
 import numpy as np
+import os
 from multiprocessing import Pool
 import tqdm
 import signal
 import time
+import ast
 
 FETCH_FROM_PANOPTES = False
 
@@ -118,15 +120,122 @@ def get_scales_set(save=True):
                 pool.join()
             except Exception as e:
                 raise(e)
-
-            pool.join()
-
+            
             run += 1
+
+        pool.join()
+
 
     if save:
         table.write('configs/subject_scales.csv', format='csv', overwrite=True)
     
     return table
 
+def modify_extracts(table, point_extracts='extracts/point_extractor_by_frame_box_the_jets.csv', 
+                    box_extracts='extracts/shape_extractor_rotateRectangle_box_the_jets.csv'):
+    points_data    = ascii.read(point_extracts, delimiter=',')
+    points_data_sc = points_data.copy()
+
+    # increase the string length so that 
+    # we don't get string truncation
+    for task in ['T1','T5']:
+        for frame in range(15):
+            for tool in ['tool0','tool1']:
+                for key in ['x','y']:
+                    col = f'data.frame{frame}.{task}_{tool}_{key}'
+                    points_data_sc[col] = points_data_sc[col].astype('<U40')
+
+
+    # loop through all the subjects
+    for row in tqdm.tqdm(table, desc='Processing points'):
+        subject = row['subject_id']
+
+        # find the relevant subjects in the point extractor
+        mask = np.where(points_data['subject_id']==subject)[0]
+
+        # for each extract
+        for row_ind in mask:
+            task = points_data['task'][row_ind]
+
+            # for each frame, tool and data value (x,y)
+            for frame in range(15):
+                scale = row[f'frame_{frame}_scale']
+                for tool in ['tool0','tool1']:
+                    for key in ['x','y']:
+
+                        # get the extract
+                        col  = f'data.frame{frame}.{task}_{tool}_{key}'
+                        data = points_data[col][row_ind]
+
+                        # check if the entry has data
+                        try:
+                            # convert to list
+                            data        = ast.literal_eval(data)
+
+                            # apply the scaling
+                            data_scaled = [val/scale for val in data]
+
+                            # and save it back to the scaled table
+                            points_data_sc[col][row_ind] = str(data_scaled)
+                            last_col = col
+                        except ValueError:
+                            continue
+    points_data_sc.write(point_extracts.replace('.csv', '_scaled.csv'), delimiter=',', overwrite=True)
+    
+    # repeat for the box data
+    box_data    = ascii.read(box_extracts, delimiter=',')
+    box_data_sc = box_data.copy()
+    
+    # increase the string length so that 
+    # we don't get string truncation
+    for task in ['T1','T5']:
+        for frame in range(15):
+            for tool in ['tool2']:
+                for key in ['x','y','width','height','angle']:
+                    col = f'data.frame{frame}.{task}_{tool}_{key}'
+                    box_data_sc[col] = box_data_sc[col].astype('<U40')
+
+
+    # loop through all the subjects
+    for row in tqdm.tqdm(table, desc='Processing box'):
+        subject = row['subject_id']
+
+        # find the relevant subjects in the point extractor
+        mask = np.where(box_data['subject_id']==subject)[0]
+
+        # for each extract
+        for row_ind in mask:
+            task = box_data['task'][row_ind]
+
+            # for each frame, tool and data value (x,y)
+            for frame in range(15):
+                scale = row[f'frame_{frame}_scale']
+                for tool in ['tool2']:
+                    for key in ['x','y','width','height']:
+
+                        # get the extract
+                        col  = f'data.frame{frame}.{task}_{tool}_{key}'
+                        data = box_data_sc[col][row_ind]
+
+                        # check if the entry has data
+                        try:
+                            # convert to list
+                            data        = ast.literal_eval(data)
+
+                            # apply the scaling
+                            data_scaled = [val/scale for val in data]
+
+                            # and save it back to the original array
+                            box_data_sc[col][row_ind] = str(data_scaled)
+                        except ValueError:
+                            continue
+                            
+    box_data_sc.write(box_extracts.replace('.csv', '_scaled.csv'), format='csv', overwrite=True)
+            
+
 if __name__=='__main__':
-    get_scales_set(False)
+    if os.path.exists('configs/subject_scales.csv'):
+        table = ascii.read('configs/subject_scales.csv')
+    else:
+        table = get_scales_set(save=True)
+    modify_extracts(table)