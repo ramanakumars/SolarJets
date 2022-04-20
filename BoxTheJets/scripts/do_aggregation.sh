b5d7172bb92caa61fda8b4d8b3f46bb6cc1a74c9
Ramanakumar Sankar <rsankar@umn.edu>
2022-04-20 12:48:18 -0500

    added command line arguments for do_aggregation

--- BoxTheJets/scripts/do_aggregation.sh
+++ BoxTheJets/scripts/do_aggregation.sh
@@ -1,5 +1,24 @@
 #!/bin/bash
 
+usage() { 
+	echo "Usage: $0 [-c <int>]"; 
+	echo "     -c : Number of processors to use for reductions [default=1]" 1>&2; 
+	exit 1; 
+}
+
+NUM_PROCS=1;
+
+while getopts ":c:" options; do 
+	case "${options}" in 
+		c) 
+			NUM_PROCS=${OPTARG}
+			;;
+		*) 
+			usage
+			;;
+	esac
+done
+
 mkdir -p {extracts,reductions}/
 
 # first get the extracts
@@ -24,7 +43,8 @@ panoptes_aggregation reduce ../extracts/point_extractor_by_frame_box_the_jets_sc
 
 # Using the new jaccard metric for clustering
 panoptes_aggregation reduce ../extracts/shape_extractor_rotateRectangle_box_the_jets_scaled_squashed.csv \
-    ../configs/Reducer_config_workflow_19650_V4.52_shape_extractor_rotateRectangle_IoU.yaml -o box_the_jets
+    ../configs/Reducer_config_workflow_19650_V4.52_shape_extractor_rotateRectangle_IoU.yaml \
+	-o box_the_jets -c ${NUM_PROCS}
 
 panoptes_aggregation reduce ../extracts/question_extractor_box_the_jets.csv\
     ../configs/Reducer_config_workflow_19650_V4.52_question_extractor.yaml -o box_the_jets
