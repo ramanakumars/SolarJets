68f082c0e492cfbacc2600d2c99ee6ab8ac2dc6b
Ramanakumar Sankar <rsankar@umn.edu>
2022-05-04 13:08:41 -0500

    working jet clustering algorithm

--- BoxTheJets/aggregation/SOL_class.py
+++ BoxTheJets/aggregation/SOL_class.py
@@ -15,7 +15,13 @@ import ast
 import os
 import datetime
 from matplotlib.dates import DateFormatter
-from aggregation import Aggregator
+import matplotlib.animation as animation
+from .workflow import Aggregator, get_subject_image
+from sklearn.cluster import OPTICS
+from scipy.spatial.distance import squareform
+from scipy.cluster.hierarchy import linkage, fcluster
+import hdbscan
+import tqdm
 
 def get_h_w_clusterbox(subject,aggregator, task='T1'):
     '''
@@ -219,5 +225,216 @@ class SOL:
             
             
             
-    
-    
+    def filter_jet_clusters(self, SOL_event):
+        # first, get a list of subjects for
+        # this event
+        subjects  = self.get_subjects(SOL_event)
+        times_all = self.get_obs_time(SOL_event)
+
+        event_jets        = []
+        jet_starts        = []
+        times             = []
+        start_confidences = []
+        # go through the subjects, and find
+        # the jets in each subject
+        for j, subject in enumerate(subjects):
+            try:
+                # find all the jets in this subject
+                jets = self.aggregator.filter_classifications(subject)
+
+                # add it to the list
+                event_jets.extend(jets)
+
+                # and also keep track of the base positions
+                jet_starts.extend([jet.start for jet in jets])
+
+                start_dist = []
+                for jet in jets:
+                    start_dist.extend(np.linalg.norm(jet.get_extract_starts() - jet.start, axis=0))
+
+                start_confidences.extend(start_dist)
+                times.extend([times_all[j] for n in range(len(jets))])
+
+            except (ValueError, IndexError):
+                continue
+
+        jets       = np.asarray(event_jets)
+        jet_starts = np.asarray(jet_starts)
+        times      = np.asarray(times)
+
+        box_metric   = np.zeros((len(jets), len(jets)))
+        time_metric  = np.zeros((len(jets), len(jets)))
+        point_metric = np.zeros((len(jets), len(jets)))
+
+        dtime = (times_all[-1] - times_all[0]).astype('timedelta64[s]').astype(float)
+
+        for j, jetj in enumerate(jets):
+            for k, jetk in enumerate(jets):
+                if j==k:
+                    point_metric[k,j] = 0
+                    box_metric[k,j]   = 0
+                    time_metric[k,j]  = 0
+                elif jetj.subject==jetk.subject:
+                    #print(j, k, jetj.subject, jetk.subject, jetj.start, jetk.start)
+                    point_metric[k,j] = np.nan#jetj.subject*np.linalg.norm(jetj.start)*np.linalg.norm(jetk.start)
+                    box_metric[k,j]   = np.nan
+                    time_metric[k,j]  = np.nan
+                else:
+                    point_dist        = np.linalg.norm((jetj.start - jetk.start))
+                    box_ious          = jetj.box.intersection(jetk.box).area/jetj.box.union(jetk.box).area
+                    point_metric[k,j] = point_dist/np.mean([start_confidences[j], start_confidences[k]])
+                    box_metric[k,j]   = 1. - box_ious
+
+                    # we will limit to 2 frames (each frame is 5 min)
+                    time_metric[k,j]  = np.abs( (times[j] - times[k]).astype('timedelta64[s]')\
+                                               .astype(float))/(10*60)
+
+        distance_metric = point_metric/np.percentile(point_metric[np.isfinite(point_metric)&(point_metric>0)], 90) + \
+                            2.*box_metric
+
+
+        distance_metric[~np.isfinite(distance_metric)] = np.nan
+
+        indices  = np.arange(len(jets))
+        labels   = -1.*np.ones(len(jets))
+        subjects = np.asarray([jet.subject for jet in jets])
+
+        eps      = 2.
+        time_eps = 2.#*np.nanmedian(distance_metric)
+
+        print(f"Using eps={eps} and time_eps={time_eps*30} min")
+
+        while len(indices) > 0:
+            ind = indices[0]
+            # this is the jet we will compare against
+            j0 = jets[ind]
+
+            # find all the jets that fall within a distance 
+            # eps for this jet and those that are not 
+            # already clustered into a jet
+            mask = (distance_metric[ind,:] < eps)&(labels==-1)
+
+            unique_subs = np.unique(subjects[mask])
+
+            # make sure that all the jets belong to different subjects
+            # two jets in the same subject should be treated differently
+            if len(unique_subs) != sum(mask):
+                # in this case, there are duplicates
+                # we will choose the best subject from each duplicate
+                count = [sum(subjects[mask]==subject) for subject in unique_subs]
+
+                # loop through the unique subs
+                for sub in unique_subs:
+                    # find the indices that correspond to this
+                    # jet in the mask
+                    inds_sub = np.where((subjects==sub)&mask)[0]
+                    # and the corresponding distances
+                    dists    = distance_metric[ind,inds_sub]
+
+                    # remove all the other subjects
+                    mask[inds_sub] = False
+
+                    # set the lowest distance index to True
+                    mask[inds_sub[np.argmin(dists)]] = True
+
+            # next make sure that there is a reachability in time
+            # jets should be connected to each other to within 1-2 frames
+            if sum(mask) > 1: # only do this when there are more than 1 jet
+                rem_inds  = np.where(mask)[0]
+                for j, indi in enumerate(rem_inds):
+                    # if this is the first index we don't 
+                    # have an idea of past reachability
+                    if j==0: 
+                        continue
+
+                    # get the reachability in time
+                    time_disti = time_metric[indi,mask]
+                    # subset it up to the current jet 
+                    # so we get only past reachability
+                    t0 = np.argmin(time_disti)
+                    time_disti = time_disti[:t0]
+                    
+                    # if the previous index was deleted
+                    # we can end up with empty lists
+                    # ignore these and assign them
+                    # to a different cluster
+                    if len(time_disti) == 0:
+                        mask[indi] = False
+                        continue
+
+                    # find the smallest interval between this jet and any other 
+                    # jet. then remove this if it more than eps frames away
+                    if time_disti[time_disti>0.].min() > time_eps:
+                        mask[indi] = False
+                
+
+            # assign a new value to these
+            labels[mask] = labels.max() + 1
+
+            rem_inds = [np.where(indices==maski)[0][0] for maski in np.where(mask)[0]]
+
+            indices = np.delete(indices, rem_inds)
+
+        # get the list of jets found
+        njets = len(np.unique(labels[labels > -1]))
+
+        assert njets > 0, "No jet clusters found!"
+
+        jet_clusters = []
+
+        for j in range(njets):
+            mask_j  = labels==j
+            # subset the list of jets that correspond to this label
+            jets_j  = jets[mask_j]
+            times_j = times[mask_j]
+
+            # for each jet, append the time information
+            for k, jet in enumerate(jets_j):
+                jet.time = times_j[k]
+
+            clusteri = JetCluster(jets_j)
+
+            jet_clusters.append(clusteri)
+
+        return jet_clusters, distance_metric, point_metric, box_metric
+
+
+class JetCluster:
+    def __init__(self, jets):
+        self.jets  = jets
+
+    def create_gif(self, output):
+        '''
+            Create a gif of the jet objects showing the 
+            image and the plots from the `Jet.plot()` method
+        '''
+        fig, ax = plt.subplots(1,1, dpi=250)
+        
+        # create a temp plot so that we can get a size estimate
+        subject0 = self.jets[0].subject
+        
+        ax.imshow(get_subject_image(subject0, 0))
+        ax.axis('off')
+        fig.tight_layout(pad=0)
+
+        # loop through the frames and plot
+        ims = []
+        for jet in tqdm.tqdm(self.jets):
+            subject = jet.subject
+            for i in range(15):
+                img = get_subject_image(subject, i)
+
+                # first, plot the image
+                im1 = ax.imshow(img)
+
+                # for each jet, plot all the details
+                # and add each plot artist to the list
+                jetims = jet.plot(ax)
+                
+                # combine all the plot artists together
+                ims.append([im1, *jetims])
+
+        # save the animation as a gif
+        ani = animation.ArtistAnimation(fig, ims)
+        ani.save(output, writer='imagemagick')
+
