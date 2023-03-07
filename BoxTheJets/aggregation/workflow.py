increased normalization for jet clustering. fixing typos

BoxTheJets/aggregation/workflow.py
@@ -328,9 +328,9 @@ class Aggregator:
         clusters['y_end'] = np.asarray(points_data['end']['clusters']['y'])
 
         clusters['prob_start'] = np.asarray(points_data['start']['extracts']['cluster_probabilities'])
-        clusters['prob_end'] = np.asarray(points_data['start']['extracts']['cluster_probabilities'])
+        clusters['prob_end'] = np.asarray(points_data['end']['extracts']['cluster_probabilities'])
         clusters['labels_start'] = np.asarray(points_data['start']['extracts']['cluster_labels'])
-        clusters['labels_end'] = np.asarray(points_data['start']['extracts']['cluster_labels'])
+        clusters['labels_end'] = np.asarray(points_data['end']['extracts']['cluster_labels'])
 
         return data, clusters
 
@@ -1382,6 +1382,10 @@ class Aggregator:
 
         # and the unique clusters
         unique_jets = self.find_unique_jets(subject)
+
+        if len(unique_jets['box']) < 1:
+            return []
+
         unique_starts, unique_ends = self.find_unique_jet_points(subject)
 
         # combine the T1 and T5 raw data