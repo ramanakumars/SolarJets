2766206893e8eb1c560d7f5f5f0e8b6478b6c054
Palomajol <palomalfjol@gmail.com>
2023-07-04 16:43:05 +0200

    fixing bugs with image handler

--- BoxTheJets/aggregation/image_handler.py
+++ BoxTheJets/aggregation/image_handler.py
@@ -6,6 +6,7 @@ import astropy.units as u
 from panoptes_client import Subject
 from sunpy.map import Map
 
+
 def world_from_pixel(subject_id, x, y):
     '''
     Gets the solar coordinates of point (x,y) on selected image
@@ -19,14 +20,17 @@ def world_from_pixel(subject_id, x, y):
     y : float
         The y value to be converted
     '''
-
+    
     subject = Subject(subject_id)
     metadata = subject.metadata
+    width = float(metadata["#width"])
+    height = float(metadata["#height"])
+    y= height-y # To account for the inverted y axis in pixel coordinates
 
     #Normalize x and y if needed
     if x > 1 and y > 1:
-        x = x / metadata['#width']
-        y = y / metadata['#height']
+        x = x / width
+        y = y / height
 
 
     #Extract metadata from subject sets with .fts headers included in their metadata.
@@ -46,9 +50,9 @@ def world_from_pixel(subject_id, x, y):
             'crval1': metadata['#crval1'], #Coordinate value at reference point on naxis1
             'crval2': metadata['#crval2'], #Coordinate value at reference point on naxis2
             'cdelt1': metadata['#cdelt1'], #Spatial scale of pixels for naxis1, i.e. coordinate increment at reference point
-            'cdelt2': metadata['#cdelt1'], #Spatial scale of pixels for naxis2, i.e. coordinate increment at reference point
+            'cdelt2': metadata['#cdelt2'], #Spatial scale of pixels for naxis2, i.e. coordinate increment at reference point
             'crpix1': metadata['#crpix1'], #Pixel coordinate at reference point naxis1
-            'crpix2': metadata['#crpix1'], #Pixel coordinate at reference point naxis2
+            'crpix2': metadata['#crpix2'], #Pixel coordinate at reference point naxis2
             'crota2': metadata['#crota2'], #Rotation of the horizontal and vertical axes in degrees
         }
 
@@ -56,12 +60,12 @@ def world_from_pixel(subject_id, x, y):
     map = Map(numpy.zeros((1,1)), fits_headers)
 
     #extract important pieces of metadata
-    fits_width = map.meta["naxis1"]
-    fits_height = map.meta["naxis2"]
-    im_ll_x = metadata['#im_ll_x']
-    im_ll_y = metadata['#im_ll_y']
-    im_ur_x = metadata['#im_ur_x']
-    im_ur_y = metadata['#im_ur_y']
+    fits_width = float(map.meta["naxis1"])
+    fits_height = float(map.meta["naxis2"])
+    im_ll_x = float(metadata['#im_ll_x'])
+    im_ll_y = float(metadata['#im_ll_y'])
+    im_ur_x = float(metadata['#im_ur_x'])
+    im_ur_y = float(metadata['#im_ur_y'])
 
     #Fit normalized pixel coordinates within the correct image box
     axis_x_normalized = (x - im_ll_x) / (im_ur_x - im_ll_x)
