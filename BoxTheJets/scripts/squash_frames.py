0a50c20fdc992548535145e249539e90a934043a
Ramanakumar Sankar <ramanakumar.shankar@gmail.com>
2023-10-12 02:46:30 -0500

    removed old code to squash frames. current frame only merges tasks

--- BoxTheJets/scripts/squash_frames.py
+++ BoxTheJets/scripts/squash_frames.py
@@ -1,169 +1,61 @@
 import numpy as np
 from astropy.io import ascii
-from astropy.table import MaskedColumn
-import ast
-import re
+import json
 
-## point extractor
-file = 'extracts/point_extractor_by_frame_box_the_jets_scaled.csv'
 
-data = ascii.read(file, delimiter=',')
+def merge_tasks(data, task1='T1', task2='T3'):
+    # fixing FixedWidth errors
+    for col in data.itercols():
+        data.replace_column(col.name, col.astype('object'))
 
-# fixing FixedWidth errors
-for col in data.itercols():
-    data.replace_column(col.name, col.astype('object'))
+    # get the col names for each variable in frame0 in T1
+    colnames = data.colnames
+    col0 = sorted([i for i in colnames if f'frame0.{task1}' in i])
 
-colnames = data.colnames
-# get the col names for each variable in frame0
-col0 = sorted([i for i in colnames if 'frame0' in i])
+    data_merged = data.copy()
 
-data['data.frame0.T1_tool0_frame'] = MaskedColumn([None]*len(data), fill_value='None',
-                                                  mask=[True]*len(data), dtype='U20')
-data['data.frame0.T1_tool1_frame'] = MaskedColumn([None]*len(data), fill_value='None',
-                                                  mask=[True]*len(data), dtype='U20')
-data['data.frame0.T5_tool0_frame'] = MaskedColumn([None]*len(data), fill_value='None',
-                                                  mask=[True]*len(data), dtype='U20')
-data['data.frame0.T5_tool1_frame'] = MaskedColumn([None]*len(data), fill_value='None',
-                                                  mask=[True]*len(data), dtype='U20')
+    for k, col0k in enumerate(col0):
 
-for k, col0k in enumerate(col0):
-    colframe = col0k.replace('_x', '_frame').replace('_y', '_frame')
-    
-    # add the frame info for classifications on frame0
-    data[col0k].fill_value = 'None'
-    mask = np.asarray(data[col0k][:].filled()) == 'None'
-    data[colframe][~mask] = str([0])
-
-    for j in range(1,15):
         # we can modify the frame0 tag to frame[n]
-        coltype = col0k.replace('frame0', 'frame%d'%j)
-        data[coltype].fill_value = 'N/A'
+        colT5 = col0k.replace(task1, task2)
+        data_merged[colT5].fill_value = 'N/A'
 
         # find the rows where there is data
-        mask = np.asarray(data[coltype][:].filled()) != 'N/A'
+        mask = np.where(np.asarray(data_merged[colT5][:].filled()) != 'N/A')[0]
 
-        # move those rows to frame0
-        data[col0[k]][mask] = data[coltype][mask]
+        for row in mask:
+            classification_id = data_merged['classification_id'][row]
+            row_T1 = np.where((data_merged['classification_id'][:] == classification_id) & (data_merged['task'][:] == task1))[0][0]
+            try:
+                dataT1 = json.loads(data_merged[col0k][row_T1])
+            except ValueError:
+                dataT1 = []
 
-        # and delete the other row
-        data[coltype][mask] = ''
+            dataT5 = json.loads(data_merged[colT5][row])
 
-        data[colframe][mask] = str([j])
+            # combine the T5 info with T1
+            outdata = []
+            outdata.extend(dataT1)
+            outdata.extend(dataT5)
 
-# get the col names for each variable in frame0 in T1
-colnames = data.colnames
-col0 = sorted([i for i in colnames if 'frame0.T1' in i])
+            # move those rows to frame0
+            data_merged[col0[k]][row_T1] = str(outdata)
+            # print(row, outdata)
 
-data_merged = data.copy()
+            # and delete the other row
+            data_merged[colT5][row] = ''
 
-for k, col0k in enumerate(col0):
-    
-    # we can modify the frame0 tag to frame[n]
-    colT5 = col0k.replace('T1', 'T5')
-    data_merged[colT5].fill_value = 'N/A'
+    return data_merged
 
-    # find the rows where there is data
-    mask = np.where(np.asarray(data_merged[colT5][:].filled()) != 'N/A')[0]
 
-    for row in mask:
-        classification_id = data_merged['classification_id'][row]
-        row_T1 = np.where((data_merged['classification_id'][:]==classification_id)&(data_merged['task'][:]=='T1'))[0][0]
-        try:
-            dataT1 = ast.literal_eval(data_merged[col0k][row_T1])
-        except ValueError:
-            dataT1 = []
+# point extractor
+points_file = 'extracts/shape_extractor_temporalPoint_box_the_jets.csv'
+points_data = ascii.read(points_file, delimiter=',')
+data_merged_points = merge_tasks(points_data, 'T0', 'T3')
+ascii.write(data_merged_points, points_file.replace('.csv', '_merged.csv'), overwrite=True, delimiter=',')
 
-        dataT5 = ast.literal_eval(data_merged[colT5][row])
-
-        # combine the T5 info with T1
-        outdata = []
-        outdata.extend(dataT1)
-        outdata.extend(dataT5)
-
-        # move those rows to frame0
-        data_merged[col0[k]][row_T1] = str(outdata)
-        # print(row, outdata)
-
-        # and delete the other row
-        data_merged[colT5][row] = ''
-
-ascii.write(data, file.replace('.csv', '_squashed.csv'), overwrite=True, delimiter=',')
-ascii.write(data_merged, file.replace('.csv', '_squashed_merged.csv'), overwrite=True, delimiter=',')
-
-## shape extractor
-file = 'extracts/shape_extractor_rotateRectangle_box_the_jets_scaled.csv'
-
-data = ascii.read(file, delimiter=',')
-
-# fixing FixedWidth errors
-for col in data.itercols():
-    data.replace_column(col.name, col.astype('object'))
-
-colnames = data.colnames
-
-col0 = sorted([i for i in colnames if 'frame0' in i])
-
-data['data.frame0.T1_tool2_frame'] = MaskedColumn([None]*len(data), fill_value='None',
-                                                  mask=[True]*len(data), dtype='U20')
-data['data.frame0.T5_tool2_frame'] = MaskedColumn([None]*len(data), fill_value='None',
-                                                  mask=[True]*len(data), dtype='U20')
-
-for k, col0k in enumerate(col0):
-    print(col0k)
-    colframe = re.sub('_([a-z]+)$', '_frame', col0k)
-
-    # add the frame info for classifications on frame0
-    data[col0k].fill_value = 'None'
-    mask = np.asarray(data[col0k][:].filled()) == 'None'
-    data[colframe][~mask] = str([0])
-
-    for j in range(1,15):
-        coltype = col0k.replace('frame0', 'frame%d'%j)
-        data[coltype].fill_value = 'None'
-        mask = np.asarray(data[coltype][:].filled()) == 'None'
-        data[col0[k]][~mask] = data[coltype][~mask]
-        data[coltype][~mask] = ''
-
-        # we are replacing this multiple times
-        # as the loop goes over the different variables
-        # (x, y, w, h and a). This should be fine, but
-        # possibly can be fixed in the future
-        data[colframe][~mask] = str([j])
-
-# get the col names for each variable in frame0 in T1
-colnames = data.colnames
-data_merged = data.copy()
-col0 = sorted([i for i in colnames if 'frame0.T1' in i])
-for k, col0k in enumerate(col0):
-    
-    # we can modify the frame0 tag to frame[n]
-    colT5 = col0k.replace('T1', 'T5')
-    data_merged[colT5].fill_value = 'N/A'
-
-    # find the rows where there is data
-    mask = np.where(np.asarray(data_merged[colT5][:].filled()) != 'N/A')[0]
-
-    for row in mask:
-        classification_id = data_merged['classification_id'][row]
-        row_T1 = np.where((data_merged['classification_id'][:]==classification_id)&(data_merged['task'][:]=='T1'))[0][0]
-        try:
-            dataT1 = ast.literal_eval(data_merged[col0k][row_T1])
-        except ValueError:
-            dataT1 = []
-
-        dataT5 = ast.literal_eval(data_merged[colT5][row])
-
-        # combine the T5 info with T1
-        outdata = []
-        outdata.extend(dataT1)
-        outdata.extend(dataT5)
-
-        # move those rows to the T1 array
-        data_merged[col0k][row_T1] = str(outdata)
-        # print(row, outdata)
-
-        # and delete the other row
-        data_merged[colT5][row] = ''
-
-ascii.write(data, file.replace('.csv', '_squashed.csv'), overwrite=True, delimiter=',')
-ascii.write(data_merged, file.replace('.csv', '_squashed_merged.csv'), overwrite=True, delimiter=',')
+# shape extractor
+box_file = 'extracts/shape_extractor_temporalRotateRectangle_box_the_jets.csv'
+box_data = ascii.read(box_file, delimiter=',')
+data_merged_box = merge_tasks(box_data, 'T0', 'T3')
+ascii.write(data_merged_box, box_file.replace('.csv', '_merged.csv'), overwrite=True, delimiter=',')
