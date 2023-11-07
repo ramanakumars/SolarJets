ad55889d3a49db1655ffa6d5ee66a63d34fedb93
Ramanakumar Sankar <ramanakumar.shankar@gmail.com>
2023-11-07 16:41:01 -0600

    switching to new output

--- BoxTheJets/scripts/create_subject_metadata.py
+++ BoxTheJets/scripts/create_subject_metadata.py
@@ -9,7 +9,7 @@ try:
 except ModuleNotFoundError:
     raise
 
-aggregator = Aggregator('reductions/point_reducer_hdbscan_box_the_jets.csv', 'reductions/shape_reducer_dbscan_box_the_jets.csv')
+aggregator = Aggregator('reductions/temporal_point_reducer_hdbscan_box_the_jets.csv', 'reductions/shape_reducer_dbscan_box_the_jets.csv')
 subjects = aggregator.get_subjects()
 metadatafile = pd.read_csv('../solar-jet-hunter-subjects.csv').to_dict(orient='list')
 for key in metadatafile.keys():
