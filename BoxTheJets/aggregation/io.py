15806c39ed2a5a189dc6a32f396942de17687cea
Ramanakumar Sankar <ramanakumar.shankar@gmail.com>
2023-11-10 15:06:55 -0600

    moved JSON subject metadata function to script

--- BoxTheJets/aggregation/io.py
+++ BoxTheJets/aggregation/io.py
@@ -3,9 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import animation
 import datetime
-import tqdm
 from .zoo_utils import get_subject_image
-from .meta_file_handler import create_subjectinfo
 
 
 class NpEncoder(json.JSONEncoder):
@@ -60,53 +58,3 @@ def create_gif(jets):
     # save the animation as a gif
     ani = animation.ArtistAnimation(fig, ims)
     ani.save(f'{subject}.gif', writer='imagemagick')
-
-
-def create_metadata_jsonfile(filename: str, subjects: np.array, subjectsdata):
-    '''
-        Write out the metadata file for a given set of subjectstoloop to filename
-        Inputs
-        ------
-        filename: str
-            Filename to where the metadata should be written to
-        subjectstoloop: np.array
-            List of subjects for which the metadata should be gathered
-        subjectsdata: astropy.table.table.Table
-            data Table with Zooniverse metadata keys ['subject_id','metadata']
-
-    '''
-
-    # Select keys we want to write to json file
-    keysToImport = [
-        '#fits_names',  # First image filename in Zooniverse subject
-        '#width',  # Width of the image in pixel
-        '#height',  # Height of the image in pixel
-        '#naxis1',  # Pixels along axis 1
-        '#naxis2',  # Pixels along axis 2
-        '#cunit1',  # Units of the coordinate increments along naxis1 e.g. arcsec
-        '#cunit2',  # Units of the coordinate increments along naxis2 e.g. arcsec
-        '#crval1',  # Coordinate value at reference point on naxis1
-        '#crval2',  # Coordinate value at reference point on naxis2
-        '#cdelt1',  # Spatial scale of pixels for naxis1, i.e. coordinate increment at reference point
-        '#cdelt2',  # Spatial scale of pixels for naxis2, i.e. coordinate increment at reference point
-        '#crpix1',  # Pixel coordinate at reference point naxis1
-        '#crpix2',  # Pixel coordinate at reference point naxis2
-        '#crota2',  # Rotation of the horizontal and vertical axes in degrees
-        '#im_ll_x',  # Vertical distance in pixels between bottom left corner and start solar image
-        '#im_ll_y',  # Horizontal distance in pixels between bottom left corner and start solar image
-        '#im_ur_x',  # Vertical distance in pixels between bottom left corner and end solar image
-        '#im_ur_y',  # Horizontal distance in pixels between bottom left corner and end solar image,
-        '#sol_standard'  # SOL event that this subject corresponds to
-    ]
-
-    subject_data = []
-    for i, subject in enumerate(tqdm.tqdm(subjects, ascii=True, desc='Writing subjects to JSON')):
-        subjectDict = {}
-        subjectDict['subject_id'] = int(subject)
-        subjectDict['data'] = create_subjectinfo(subject, subjectsdata, keysToImport)
-        subject_data.append(subjectDict)
-
-    with open(filename, 'w') as outfile:
-        json.dump(subject_data, outfile, cls=NpEncoder)
-
-    print(f"Wrote subject information to {filename}")
--- BoxTheJets/scripts/create_subject_metadata.py
+++ BoxTheJets/scripts/create_subject_metadata.py
@@ -1,18 +1,56 @@
 import numpy as np
 import pandas as pd
 import sys
+import tqdm
+import json
 sys.path.append('.')
 
 try:
     from aggregation.workflow import Aggregator
-    from aggregation.io import create_metadata_jsonfile
+    from aggregation.meta_file_handler import create_subjectinfo
+    from aggregation.io import NpEncoder
 except ModuleNotFoundError:
     raise
 
 aggregator = Aggregator('reductions/temporal_point_reducer_hdbscan_box_the_jets.csv', 'reductions/shape_reducer_dbscan_box_the_jets.csv')
 subjects = aggregator.get_subjects()
-metadatafile = pd.read_csv('../solar-jet-hunter-subjects.csv').to_dict(orient='list')
-for key in metadatafile.keys():
-    metadatafile[key] = np.asarray(metadatafile[key])
+subject_metadata = pd.read_csv('../solar-jet-hunter-subjects.csv').to_dict(orient='list')
+for key in subject_metadata.keys():
+    subject_metadata[key] = np.asarray(subject_metadata[key])
 
-create_metadata_jsonfile('../solar_jet_hunter_subject_metadata.json', subjects, metadatafile)
+filename = '../solar_jet_hunter_metadata.json'
+
+# Select keys we want to write to json file
+keysToImport = [
+    '#fits_names',  # First image filename in Zooniverse subject
+    '#width',  # Width of the image in pixel
+    '#height',  # Height of the image in pixel
+    '#naxis1',  # Pixels along axis 1
+    '#naxis2',  # Pixels along axis 2
+    '#cunit1',  # Units of the coordinate increments along naxis1 e.g. arcsec
+    '#cunit2',  # Units of the coordinate increments along naxis2 e.g. arcsec
+    '#crval1',  # Coordinate value at reference point on naxis1
+    '#crval2',  # Coordinate value at reference point on naxis2
+    '#cdelt1',  # Spatial scale of pixels for naxis1, i.e. coordinate increment at reference point
+    '#cdelt2',  # Spatial scale of pixels for naxis2, i.e. coordinate increment at reference point
+    '#crpix1',  # Pixel coordinate at reference point naxis1
+    '#crpix2',  # Pixel coordinate at reference point naxis2
+    '#crota2',  # Rotation of the horizontal and vertical axes in degrees
+    '#im_ll_x',  # Vertical distance in pixels between bottom left corner and start solar image
+    '#im_ll_y',  # Horizontal distance in pixels between bottom left corner and start solar image
+    '#im_ur_x',  # Vertical distance in pixels between bottom left corner and end solar image
+    '#im_ur_y',  # Horizontal distance in pixels between bottom left corner and end solar image,
+    '#sol_standard'  # SOL event that this subject corresponds to
+]
+
+subject_data = []
+for i, subject in enumerate(tqdm.tqdm(subjects, ascii=True, desc='Writing subjects to JSON')):
+    subjectDict = {}
+    subjectDict['subject_id'] = int(subject)
+    subjectDict['data'] = create_subjectinfo(subject, subject_metadata, keysToImport)
+    subject_data.append(subjectDict)
+
+with open(filename, 'w') as outfile:
+    json.dump(subject_data, outfile, cls=NpEncoder)
+
+print(f"Wrote subject information to {filename}")
