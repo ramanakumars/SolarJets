764e65683bc1436d4a8a8e397a27e1cb4988b6c3
Ramanakumar Sankar <ramanakumar.shankar@gmail.com>
2023-11-14 19:39:15 -0600

    refactored code for readability

--- scripts/squash_frames.py
+++ scripts/squash_frames.py
@@ -17,21 +17,21 @@ def merge_tasks(data, task1='T1', task2='T3'):
     for k, col0k in enumerate(col0):
 
         # we can modify the frame0 tag to frame[n]
-        colT5 = col0k.replace(task1, task2)
-        data_merged[colT5].fill_value = 'N/A'
+        col2 = col0k.replace(task1, task2)
+        data_merged[col2].fill_value = 'N/A'
 
         # find the rows where there is data
-        mask = np.where(np.asarray(data_merged[colT5][:].filled()) != 'N/A')[0]
+        mask = np.where(np.asarray(data_merged[col2][:].filled()) != 'N/A')[0]
 
         for row in mask:
             classification_id = data_merged['classification_id'][row]
-            row_T1 = np.where((data_merged['classification_id'][:] == classification_id) & (data_merged['task'][:] == task1))[0][0]
+            row_col1 = np.where((data_merged['classification_id'][:] == classification_id) & (data_merged['task'][:] == task1))[0][0]
             try:
-                dataT1 = json.loads(data_merged[col0k][row_T1])
-            except ValueError:
+                dataT1 = json.loads(data_merged[col0k][row_col1])
+            except (ValueError,TypeError):
                 dataT1 = []
 
-            dataT5 = json.loads(data_merged[colT5][row])
+            dataT5 = json.loads(data_merged[col2][row])
 
             # combine the T5 info with T1
             outdata = []
@@ -39,11 +39,11 @@ def merge_tasks(data, task1='T1', task2='T3'):
             outdata.extend(dataT5)
 
             # move those rows to frame0
-            data_merged[col0[k]][row_T1] = str(outdata)
+            data_merged[col0[k]][row_col1] = str(outdata)
             # print(row, outdata)
 
             # and delete the other row
-            data_merged[colT5][row] = ''
+            data_merged[col2][row] = ''
 
     return data_merged
 
